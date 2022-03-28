c87bfa25af920f229d558a86e91e9905dd04eb12
KSroido <ksxmyqj@gmail.com>
2022-03-28 18:19:05 +0800

    change last upload slightly(some string)

--- blogDevBranchAPP/apps.py
+++ blogDevBranchAPP/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BlogdevbranchappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'BlogDevBranchAPP'
+    name = 'blogDevBranchAPP'
